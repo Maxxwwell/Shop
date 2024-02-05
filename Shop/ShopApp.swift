08cdce92d9ee535461ce718d4da29319b4d15f03
Maxwell <109968762+agyei-mtn-gh@users.noreply.github.com>
2024-02-05 14:42:58 +0000

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Maxwell on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
